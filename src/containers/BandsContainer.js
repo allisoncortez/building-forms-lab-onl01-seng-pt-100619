b29b0b68a0ca8e3ad0a324505cfa3ac16fc8197b
allisoncortez <allizetorc@gmail.com>
2020-08-01 16:50:32 -0500

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
+import Bands from '../components/Bands'
+import { connect } from 'react-redux'
+
+//acts as: connection to the Redux store // child of App 
+
+// The dispatched action required to initiate this update
+// should be set up in BandsContainer and passed down as the prop addBand
+// to BandInput.
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <Bands bands={ this.props.bands }/>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = ({bands}) => ({bands})
+
+// takes in dispatch as an argument
+// It then returns an object that contains a function as a value!
+const mapDispatchToProps = dispatch => ({
+  addBand: band => dispatch({ type: "ADD_BAND", band})
+})
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem())
+//     }
+//   };
+// };
+
+export default connect (mapStateToProps, mapDispatchToProps) (BandsContainer)
+
+// we can pass connect() a second argument, and add our action creator as props
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,11 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  switch (action.type){
+    //concat a new band (each time the action is dispatched)
+    case 'ADD_BAND':
+      return {...state, bands: [...state.bands, action.band]}
+    default:
+      return state
+  }
 };
